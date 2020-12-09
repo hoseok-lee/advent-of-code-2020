f6306bc6971ed1f4abaa40bd65cfd9f2299bb9f4
hoseok-lee <47509132+hoseok-lee@users.noreply.github.com>
2020-12-09 04:31:02 -0500

    Day 2 completed
    Day 2 of Advent of Code 2020 completed

--- day-2/day-2.py
+++ day-2/day-2.py
@@ -1,23 +1,65 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Password:
-    policy_min: str
-    policy_max: str
-    policy_str: str
-    password:   str
-
-
 '''
     PROCESS INPUTS
 '''
 
 # Open and parse input text
 f = open("passwords.txt", "r")
-unprocessed_input = list(f.read().split("\n"))
+password_templates = list(f.read().split("\n"))
 
-for password_template in unprocessed_input:
+
+
+'''
+    PART 1: OLD POLICY
+'''
+'''
+valid_passwords = 0
+
+for password_template in password_templates:
     policy, letter, password = password_template.split(" ")
+
+    # Strip the ":" from the letter
+    letter = letter[:-1]
+
+    # Determine minimum and maximum occurence of letter
+    policy_min, policy_max = [int(x) for x in policy.split("-")]
+
+    # Count how many occurences of letter in password
+    occurence = password.count(letter)
+
+    # Determine validity
+    if (occurence >= policy_min) and (occurence <= policy_max):
+        valid_passwords += 1
+
+print(valid_passwords)
+'''
+
+
+'''
+    PART 2: NEW POLICY
+'''
+
+valid_passwords = 0
+
+for password_template in password_templates:
+    policy, letter, password = password_template.split(" ")
+
+    # Strip the ":" from the letter
+    letter = letter[:-1]
+
+    # Determine first and second position of letter occurence
+    policy_one, policy_two = [int(x) for x in policy.split("-")]
+
+    # Determine validity
+    # This is the easiest method I could think of that requires a low (and consistent) amount of comparisons
+    flag = 0
     
-    
+    if password[policy_one - 1] == letter:
+        flag += 1
+
+    if password[policy_two - 1] == letter:
+        flag += 1
+
+    if flag == 1:
+        valid_passwords += 1
+
+print(valid_passwords)
