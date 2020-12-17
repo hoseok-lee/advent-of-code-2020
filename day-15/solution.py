6299ff8d4bfee2c4ba99c523fad796bf1481afd1
hoseok-lee <47509132+hoseok-lee@users.noreply.github.com>
2020-12-17 14:03:04 -0500

    Day 15 completed
    Day 15 of Advent of Code 2020 completed

---
+++ day-15/solution.py
@@ -0,0 +1,54 @@
+'''
+    PROCESS INPUTS
+'''
+
+# Open and parse input text
+f = open("starting-numbers.txt", "r")
+starting_numbers = [
+    int(number)
+    
+    for number in f.read().split(",")
+]
+
+
+
+'''
+    PART 1 + 2: PROCESS MEMORY GAME
+
+    Complexity: O(n)
+
+    The algorithm keeps a dictionary of numbers encountered so far and stores
+    the last turn number that the number was mentioned 
+'''
+
+# Current and next number to generate
+current_number = None
+next_number = None
+
+# Turn of the numbers
+turn_numbers = {}
+
+# Iterate through the turns from 1 to 2020
+for turn_number in range(1, (30000000 + 1)):
+    # At the start, pop all the numbers from the starting numbers
+    # This will overwrite the new number that was generated from last pass
+    if (turn_number - 1) < len(starting_numbers):
+        next_number = starting_numbers[turn_number - 1]
+
+    # Retrieve the number that was generated last pass 
+    current_number = next_number
+
+    # If the current number has not yet been recorded
+    if current_number not in turn_numbers:
+        # Force the next number to be 0
+        next_number = 0
+
+    # Otherwise, calculate the age of the number using the dictionary
+    else:
+        next_number = (turn_number - turn_numbers[current_number])
+
+    # Record in the dictionary
+    # Note that this step is the same whether the current number is new or not
+    turn_numbers[current_number] = turn_number
+
+print(current_number)
