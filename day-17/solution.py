5ab5754ca6f73a37e19274d64c5b97c791baf4a2
hoseok-lee <47509132+hoseok-lee@users.noreply.github.com>
2020-12-20 17:28:11 -0500

    Modified day 17
    Day 17 now uses convolution instead of brute force set-search of coordinates

--- day-17/solution.py
+++ day-17/solution.py
@@ -1,7 +1,11 @@
-from itertools import product
+import numpy as np
+from scipy.ndimage import convolve
 
-# Dimensions
+# Number of dimensions
 __DIMENSIONS__ = 4
+
+# Neighbour check kernel
+__KERNEL__ = np.ones((3,) * __DIMENSIONS__)
 
 
 
@@ -11,51 +15,41 @@ __DIMENSIONS__ = 4
 
 # Open and parse input text
 with open("initial-state.txt", "r") as f:
-    raw_file = f.read()
+    raw_lines = f.read()
 
-# Generate cube locations at tuples
-universe = {
-    (x, y) + (0,) * (__DIMENSIONS__ - 2)
-    
-    for y, row in enumerate(raw_file.split("\n"))
-    for x, cell in enumerate(list(row))
-    if (cell == "#")
-}
+# Import universe
+universe = np.array([
+    [
+        int(cell == "#")
+
+        for cell in row
+    ]
+
+    for row in raw_lines.split("\n")
+])
 
 
 
 '''
-    PART 1 + 2: GENERATE CUBE FORMATIONS
+    PART 1 + 2: SIMULATE CUBES
 
-    The algorithm only keeps a set of the coordinates of the currently occupied
-    cube locations. At every pass, the algorithm checks every possible
-    coordinate, looks at their neighbours, and checks if the coordinate exists
-    in the list of cube locations (i.e. a cube is at that location). This is how
-    the number of neighbours are determined. 
-
-    filter() applies this neighbour check at every location and generates a new
-    set of cube locations. 
+    This algorithm uses convolution to generate an adjacency matrix of nearest
+    neighbours, and generates a new universe for the next pass. 
 '''
 
-# Check neighbours
-def check_neighbours (cube):
-    # Generate list of all neighbouring coordinates
-    neighbour_cubes = [range(_ - 1, _ + 2) for _ in cube]
+# Reshape universe for convolution
+# Over-pad for constant convolution
+universe = np.pad(universe[(None,) * (__DIMENSIONS__ - universe.ndim)], 10)
 
-    # Generate list of nearby active cubes, by intersecting with all actives
-    neighbours = len(universe & set(product(*neighbour_cubes)))
-
-    # Note that neighbours count original cube
-    return ((cube in universe) and (neighbours == 4)) or (neighbours == 3)
-
-
-
-# Iterate through 6 passes
 for _ in range(6):
-    universe = set(filter(
-        check_neighbours, 
-        
-        product(range(-(_ + 10), (_ + 10)), repeat=__DIMENSIONS__)
-    ))
+    # Convolve with neighbour kernel
+    neighbours = convolve(universe, __KERNEL__, mode="constant")
 
-print(len(universe))
+    # Generate a new universe with neighbour constraints
+    universe[:] = np.where(
+        (universe & (neighbours == 4)) |
+        (neighbours == 3), 
+        1, 0
+    )
+
+print(np.sum(universe))
